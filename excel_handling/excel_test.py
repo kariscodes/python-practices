477ada9c86426383225c642a139861a9037eec54
kariscodes <kyungho.lim@gmail.com>
2020-04-28 17:11:01 +0900

    initial commit

---
+++ excel_handling/excel_test.py
@@ -0,0 +1,24 @@
+#print 'hello world!'
+from openpyxl import Workbook
+wb = Workbook()
+
+# grab the active worksheet
+ws = wb.active
+
+# Data can be assigned directly to cells
+ws['A1'] = 42
+
+# Rows can also be appended
+ws.append([1, 2, 3])
+
+# Python types will automatically be converted
+import datetime
+ws['A2'] = datetime.datetime.now()
+
+# Save the file
+wb.save("reference.xlsx")
+
+print("Complete!")
+
+# 라인 정지
+a = input()
